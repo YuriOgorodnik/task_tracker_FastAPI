62c0a4d34d859f109d4274d0de3934f3a466e71b
User <yuri.ogorodnik@mail.ru>
2023-12-27 18:49:19 +0300

    added environment variables for the mail service

--- src/config.py
+++ src/config.py
@@ -3,8 +3,19 @@ import os
 
 load_dotenv()
 
-DB_NAME = os.environ.get('DB_NAME')
-DB_USER = os.environ.get('DB_USER')
-DB_PASS = os.environ.get('DB_PASS')
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+
+DB_HOST_TEST = os.environ.get("DB_HOST_TEST")
+DB_PORT_TEST = os.environ.get("DB_PORT_TEST")
+DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
+DB_USER_TEST = os.environ.get("DB_USER_TEST")
+DB_PASS_TEST = os.environ.get("DB_PASS_TEST")
+
+SMTP_HOST = os.getenv("SMTP_HOST")
+SMTP_PORT = os.getenv("SMTP_PORT")
+SMTP_USER = os.getenv("SMTP_USER")
+SMTP_PASS = os.getenv("SMTP_PASS")
