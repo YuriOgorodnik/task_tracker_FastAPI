b02545d97e83dbbbd7bfdf24e32e1d302fa6e74b
User <yuri.ogorodnik@mail.ru>
2023-12-27 19:04:34 +0300

    update files

--- src/employee/schemas.py
+++ src/employee/schemas.py
@@ -3,13 +3,13 @@ from typing import Optional
 
 
 class EmployeeRead(BaseModel):
-    id: int
-    last_name: str
-    first_name: str
-    patronymic: Optional[str] = None
-    position: str
-    phone_number: str
-    email: str
-    address: str
-    city: str
-    country: str
+    id: int  # Поле для уникального идентификатора сотрудника
+    last_name: str  # Поле для фамилии сотрудника
+    first_name: str  # Поле для имени сотрудника
+    patronymic: Optional[str] = None  # Необязательное поле для отчества сотрудника
+    position: str  # Поле для должности сотрудника
+    phone_number: str  # Поле для номера телефона сотрудника
+    email: str  # Поле для электронной почты сотрудника
+    address: str  # Поле для адреса проживания сотрудника
+    city: str  # Поле для города проживания сотрудника
+    country: str  # Поле для страны проживания сотрудника
--- src/task/schemas.py
+++ src/task/schemas.py
@@ -5,19 +5,23 @@ from typing import Optional
 
 
 class TaskCreate(BaseModel):
-    title: str
-    created_at: datetime
-    deadline: datetime
-    is_active: bool
-    parent_task_id: Optional[int] = None
-    employee_id: Optional[int]
+    title: str  # Поле для названия задачи
+    created_at: datetime  # Поле для даты и времени создания задачи
+    deadline: datetime  # Поле для дедлайна задачи
+    is_active: bool  # Поле для статуса активности задачи
+    parent_task_id: Optional[
+        int
+    ] = None  # Необязательное поле для идентификатора родительской задачи
+    employee_id: Optional[int]  # Необязательное поле для идентификатора сотрудника
 
 
 class TaskRead(BaseModel):
-    id: int
-    title: str
-    created_at: datetime
-    deadline: datetime
-    is_active: bool
-    parent_task_id: Optional[int] = None
-    employee_id: Optional[int]
+    id: int  # Поле для уникального идентификатора задачи
+    title: str  # Поле для названия задачи
+    created_at: datetime  # Поле для даты и времени создания задачи
+    deadline: datetime  # Поле для дедлайна задачи
+    is_active: bool  # Поле для статуса активности задачи
+    parent_task_id: Optional[
+        int
+    ] = None  # Необязательное поле для идентификатора родительской задачи
+    employee_id: Optional[int]  # Необязательное поле для идентификатора сотрудника
